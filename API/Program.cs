56d701af0efc0aeabc3d529aeba5a39b497d08db
Juan Steven <jusmontenegrope@gmail.com>
2022-10-30 16:03:53 -0500

    #creacion de interfaces

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,15 @@
+using Core.Interfaces;
+using Infraestructura;
+using Infraestructura.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
 var app = builder.Build();
+
+builder.Services.AddScoped<IPublicacionRepository, PublicacionRepository>();
+
+builder.Services.AddSingleton<DapperContext>();
 
 app.UseHttpsRedirection();
 
